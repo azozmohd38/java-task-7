79a132e93295663238317a4f2598901a691b57ff
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:59:32 +0400

    fix menu choice input

--- src/StudentNameManager.java
+++ src/StudentNameManager.java
@@ -60,6 +60,12 @@ public class StudentNameManager {
                 System.out.println("7. Exit");
                 System.out.print("Enter your choice: ");
 
+                while (!input.hasNextInt()) {
+                    System.out.println("Invalid menu choice.");
+                    input.next();
+                    System.out.print("Enter your choice: ");
+                }
+
                 choice = input.nextInt();
                 input.nextLine();
 
