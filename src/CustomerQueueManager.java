aa8d8e426c1b6d7b731ab1bc22838bf22cf5b3c9
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 16:07:02 +0400

    Task 369 - fix customer count input

--- src/CustomerQueueManager.java
+++ src/CustomerQueueManager.java
@@ -19,6 +19,12 @@ public class CustomerQueueManager {
 
         // Ask the user for the number of customers
         System.out.print("Enter the number of customers: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid number of customers.");
+            scanner.next();
+            System.out.print("Enter the number of customers: ");
+        }
+
         numberOfCustomers = scanner.nextInt();
         scanner.nextLine();
 
