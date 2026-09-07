76bc8c838223c12b8ee365d7b45dc3c6640cb402
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 16:09:16 +0400

    Task 371 - number ticket customers

--- src/TicketQueueManager.java
+++ src/TicketQueueManager.java
@@ -116,8 +116,10 @@ public class TicketQueueManager {
                             System.out.println("Queue is empty.");
                         } else {
                             System.out.println("Customers in the queue:");
+                            int position = 1;
                             for (String customer : customerQueue) {
-                                System.out.println(customer);
+                                System.out.println(position + ". " + customer);
+                                position++;
                             }
                         }
                         break;
